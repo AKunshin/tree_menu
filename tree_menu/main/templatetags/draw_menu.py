ae91c96530fdab1818946f8791401a355c4d787e
AKunshin <reyiby2015@gmail.com>
2023-03-06 10:41:39 +0300

    A branch is being built for item. For root elements still in the works

--- tree_menu/main/templatetags/draw_menu.py
+++ tree_menu/main/templatetags/draw_menu.py
@@ -9,23 +9,20 @@ register = template.Library()
 @register.inclusion_tag("main/menu_list.html", takes_context=True)
 def draw_menu(context, main_menu):
     item = context["main_menu"]
-
     logger.debug(f"Item: {item}")
-
     root_menu_items = MenuItem.objects.filter(nesting_level=1)
 
 
-
-    def get_submenu(object) -> object:
-        """Фуннкция для нахождения всех предков для объекта меню"""
-        branch = [object.childs.all(), object]
+    def get_submenu(object) -> list:
+        """Функция для нахождения всех предков для объекта меню"""
         if object.parrent:
+            branch = [object.childs.all(), object]
             submenu = object.parrent
-            if submenu.nesting_level == 1:
-                return branch
-            else:
+            if submenu.nesting_level > 1:
                 branch.append(submenu)
                 get_submenu(submenu)
+        else:
+            branch = [object.childs.all()]
         logger.debug(f"Get submenu: {branch}")
         return branch
 
@@ -34,10 +31,12 @@ def draw_menu(context, main_menu):
         """Функция для построения списка всех объектов меню"""
         for root_item in root_menu_items:
             for second_item in root_item.childs.all():
-                logger.debug(f"Second_item: {second_item}")
-                if second_item in get_submenu(item):
+                logger.debug(f"second_item: {second_item}")
+                logger.debug(f"get submenu item: {get_submenu(item)[-1]}")
+                if second_item == get_submenu(item).pop():
                     root_item.children = get_submenu(item)
                     logger.debug(f"Root_item_children: {root_item.children}")
+                    return root_menu_items
         logger.debug(f"Root_menu_items: {root_menu_items}")
         return root_menu_items
 
