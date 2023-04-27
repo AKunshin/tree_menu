7056404fd18986b999385069d8455f0c5f74e650
AKunshin <reyiby2015@gmail.com>
2023-04-27 16:20:49 +0300

    I tried to specify descendants and parents for objects

--- tree_menu/main/templatetags/draw_menu.py
+++ tree_menu/main/templatetags/draw_menu.py
@@ -1,4 +1,5 @@
 from django import template
+from loguru import logger
 
 from main.models import MenuItem
 
@@ -10,30 +11,59 @@ def draw_menu(context, main_menu):
     item = context["main_menu"]
     root_menu_items = MenuItem.objects.filter(nesting_level=1)
 
-    branch = [child for child in item.childs.all()]
-    branch.append(item)
+    logger.debug(f"Сам item: {item}")
 
-    def get_submenu(object) -> list:
+
+    # item.children = item.childs.all()
+    # _branch = list(item)
+    #Объявляем свойство childern - список дочерних пунктов
+
+    # logger.debug(f"branch: {branch}")
+    # logger.debug(f"item.children: {item.children}")
+    _branch = []
+    def get_upper_level_menu(object):
         """Builds a list of all the objects parents up to the root"""
-        submenu_item = object.parrent
-        if submenu_item:
-            if submenu_item.nesting_level >= 1:
-                branch.append(submenu_item)
-                get_submenu(submenu_item)
-        return branch
 
-    def build_menu_tree(objects) -> list:
+        logger.debug(f"object in get_upper_level_menu: {object}")
+        object.children = object.childs.all()
+        _branch.append(object)
+        logger.debug(f"branch: {_branch}")
+
+        upp_level_menu_item = object.parrent
+        logger.debug(f"upp_level_menu_item: {upp_level_menu_item}")
+        if upp_level_menu_item:
+            if upp_level_menu_item.nesting_level >= 1:
+
+                upp_level_menu_item.children = upp_level_menu_item.childs.all()
+                logger.debug(f"upp_level_menu_item.children: {upp_level_menu_item.children}")
+                get_upper_level_menu(upp_level_menu_item)
+        logger.debug(f"branch: {_branch}")
+        return _branch
+
+    def build_menu_tree(objects):
         """Builds a list of all menu objects"""
-        submenu = get_submenu(item)
-        #The submenu is built including the root parent element
-        root_item_for_submenu = submenu.pop()
-        #Returning the root parent from the list
-
+        _branch = get_upper_level_menu(item)
+        logger.debug(f"_branch: {_branch}")
+        root_item_for_item = _branch.pop()
+        _branch.reverse()
+        logger.debug(f"root_item_for_item: {root_item_for_item}")
         for root_item in root_menu_items:
-            if root_item == root_item_for_submenu:
-                submenu.reverse()
-                root_item.children = submenu
+            if root_item == root_item_for_item:
+                root_item.children = root_item_for_item.childs.all()
+                logger.debug(f"root_item.children: {root_item.children}")
+        logger.debug(f"root_menu_items: {root_menu_items}")        
         return root_menu_items
 
+    menu_list = build_menu_tree(root_menu_items)
+    logger.debug(f"menu_list {menu_list}")
 
-    return {"menu_list": build_menu_tree(root_menu_items)}
+    for item in menu_list:
+        try:
+            logger.debug(f"item.children: {item.children}")
+            for it1 in item.children:
+                logger.debug(f"it1.children: {it1.children}")
+        except:
+            logger.debug(f"у {item} нет потомков")
+
+
+    return {"menu_list": menu_list}
