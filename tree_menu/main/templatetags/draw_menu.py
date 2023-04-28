d2bd084ad96d43aa3aa7faaa2f910ea76f027ac5
AKunshin <reyiby2015@gmail.com>
2023-04-28 08:33:03 +0300

    changed the variable name to a more logical

--- tree_menu/main/templatetags/draw_menu.py
+++ tree_menu/main/templatetags/draw_menu.py
@@ -1,4 +1,5 @@
 from django import template
+from loguru import logger
 
 from main.models import MenuItem
 
@@ -10,16 +11,20 @@ def draw_menu(context, main_menu):
     item = context["main_menu"]
     root_menu_items = MenuItem.objects.filter(nesting_level=1)
 
-    branch = [children for children in item.childs.all()]
+    branch = list(item.childs.all())
+
+    logger.debug(f"branch: {branch}")
+
     branch.append(item)
 
     def get_submenu(object) -> list:
         """Builds a list of all the objects parents up to the root"""
-        submenu_item = object.parrent
-        if submenu_item:
-            if submenu_item.nesting_level >= 1:
-                branch.append(submenu_item)
-                get_submenu(submenu_item)
+        under_menu_item = object.parrent
+        if under_menu_item:
+            # if under_menu_item.nesting_level >= 1:
+            branch.append(under_menu_item)
+            get_submenu(under_menu_item)
+        logger.debug(f"branch: {branch}")
         return branch
 
     def build_menu_tree(objects) -> list:
@@ -33,7 +38,8 @@ def draw_menu(context, main_menu):
             if root_item == root_item_for_submenu:
                 submenu.reverse()
                 root_item.children = submenu
+        logger.debug(f"root_menu_items: {root_menu_items}")
         return root_menu_items
 
-
-    return {"menu_list": build_menu_tree(root_menu_items)}
+    menu_list = build_menu_tree(root_menu_items)
+    return {"menu_list": menu_list}
