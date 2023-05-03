3103022f3f4b893725a3fded18bf85d267518551
AKunshin <reyiby2015@gmail.com>
2023-05-03 08:42:33 +0300

    Divided the template for menu into 2 parts. The menu is drawn correctly

--- tree_menu/main/templatetags/draw_menu.py
+++ tree_menu/main/templatetags/draw_menu.py
@@ -6,35 +6,32 @@ from main.models import MenuItem
 register = template.Library()
 
 
-@register.inclusion_tag("main/menu_list.html", takes_context=True)
+@register.inclusion_tag("main/root_menu.html", takes_context=True)
 def draw_menu(context, main_menu):
     active_item = context["main_menu"]
     logger.debug(f"active_item {active_item}")
 
     main_items = MenuItem.objects.filter(nesting_level=1)
     logger.debug(f"main_items {main_items}")
+    branch=[active_item]
 
-    def get_root_item(object):
+
+    def get_active_branch(object) -> list:
         """Builds a list of all the objects parents up to the root"""
-        logger.debug(f"object {object}")
-        parrent = object.parrent
-        logger.debug(f"parrent {parrent}")
-
-        if not parrent:
-            return object
-        else:
-            return get_root_item(parrent)
+        under_menu_item = object.parrent
+        if under_menu_item:
+            branch.append(under_menu_item)
+            get_active_branch(under_menu_item)
+        logger.debug(f"branch: {branch}")
+        return branch
         
-    
-    root_item = get_root_item(active_item)
+    active_branch = get_active_branch(active_item)
+    root_item = active_branch[-1]
     logger.debug(f"root_item: {root_item}")
-    children = root_item.childs.all()
-    logger.debug(f"children {children}")
 
 
     return {
         "main_items": list(main_items),
-        "root_item": root_item,
-        "active_item": active_item,
-        # "children": children,
+        "active_item": active_item, 
+        "active_branch": active_branch,
                }
